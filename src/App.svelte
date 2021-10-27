ee7ae2a53f11d13dc8edd14e86a365c2498b5bad
Kin Yuunan <yuunan.kin@gmail.com>
2021-10-27 19:08:45 +0900

    show stats of feedbacks (using state)

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import FeebackStats from "./components/FeebackStats.svelte";
+
 	import FeedbackList from "./components/FeedbackList.svelte";
 	import type { Feedback } from "./type.svelte";
 
@@ -19,6 +21,13 @@
 			text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.",
 		},
 	];
+
+	$: count = feedbacks.length;
+	$: average =
+		count == 0
+			? 0
+			: feedbacks.reduce((a, { rating }) => a + rating, 0) / count;
+
 	const deleteFeedback = (e: CustomEvent<number>) => {
 		const id = e.detail;
 		feedbacks = feedbacks.filter((f) => f.id !== id);
@@ -26,5 +35,6 @@
 </script>
 
 <main class="container">
+	<FeebackStats {count} {average} />
 	<FeedbackList {feedbacks} on:delete-feedback={deleteFeedback} />
 </main>
---
+++ src/components/FeebackStats.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    export let count;
+    export let average;
+</script>
+
+<div class="feedback-stats">
+    <h4>{count} Reviews.</h4>
+    <h4>Average Rating:{average}</h4>
+</div>
+
+<style>
+    .feedback-stats {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+    }
+</style>
